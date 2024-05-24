26a208657e1fd1b6b7cf469e4420e90352beec60
Jordan Daniel 27 <jordanmoreano.ppy@gmail.com>
2024-05-23 19:07:34 -0500

    cambios 1.0

--- resources/views/pages/products/create.blade.php
+++ resources/views/pages/products/create.blade.php
@@ -156,7 +156,7 @@
                                             </svg>
 
                                         </div>
-                                        <input type="number" id="precio" name="precio" value=""
+                                        <input type="number" id="precio" name="precio" value="" step="0.1"
                                             class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                             placeholder="precio">
                                     </div>
