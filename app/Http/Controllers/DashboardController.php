87d7994d2d185f2d976b15b72f37b8f6d276c0d8
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-07-12 11:47:41 -0500

    NTS

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -25,8 +25,8 @@ class DashboardController extends Controller
             ->orderBy('month', 'desc')
             ->get();
 
-        $salesThisMonth = $sales->get(0) ?? json_decode('{}', false);
-        $salesLastMonth = $sales->get(1) ?? json_decode('{}', false);
+        $salesThisMonth = $sales->get(0) ?? json_decode('{"month": null}', false);
+        $salesLastMonth = $sales->get(1) ?? json_decode('{"month": null}', false);
 
         $date = Carbon::createFromFormat('Y-m', $salesThisMonth->month);
         $salesThisMonth->month = $date->locale('es')->translatedFormat('F Y');
